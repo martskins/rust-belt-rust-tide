6a69640ea91c34b08d787514126004d8cec314e7
Martin Asquino <martin.asquino@gmail.com>
2019-07-09 10:08:23 -0300

    Hello world app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(async_await, async_closure)]
+
+fn main() -> Result<(), std::io::Error> {
+    let mut app = tide::App::new();
+    app.at("/").get(async move |_| "Hello, world!");
+    Ok(app.run("127.0.0.1:8000")?)
 }
